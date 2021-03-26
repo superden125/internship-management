669103dbfb9031139d082801b16f9120257c8b9f
Huy <tranthanhhuy2207@gmail.com>
2021-03-26 10:48:00 +0700

    Custom layouts student/home

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ import { Router } from "express";
 const router = Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home", { title: "Internship Management System" });
+  res.render("student/home", {
+    title: "Internship Management System",
+    roleName: 'Sinh viên',
+    urlInfo: 'Thông tin thực tập'
+  });
 });
 
 module.exports = router;
